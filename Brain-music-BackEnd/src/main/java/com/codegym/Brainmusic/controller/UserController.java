b391d02056f522662c88470ff777a2ff551baa5a
Lycifer00 <lamanhb97@gmail.com>
2019-03-01 15:39:12 +0700

    Add Cross origin

--- Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/controller/UserController.java
+++ Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
--- Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/repository/UserRepository.java
+++ Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@@Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
