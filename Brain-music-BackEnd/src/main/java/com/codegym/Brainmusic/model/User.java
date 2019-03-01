e74afc9a54ddc6a33d23415e580f637471f9f4be
trunglk16 <trungnguyenlk16@gmail.com>
2019-03-01 13:45:03 +0700

    Fixbug - Import library

--- Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/model/User.java
+++ Brain-music-BackEnd/src/main/java/com/codegym/Brainmusic/model/User.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
-
 @Entity
 @Table(name = "user")
 @EntityListeners(AuditingEntityListener.class)
