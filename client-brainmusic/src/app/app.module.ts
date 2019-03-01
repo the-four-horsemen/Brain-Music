dade8217bbec68b4735e9179e036b79b845a876b
HUNGDT <hk2t.thaihung@gmail.com>
2019-03-01 15:03:49 +0700

    validate + form

--- client-brainmusic/src/app/app.module.ts
+++ client-brainmusic/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { RegisterFormComponent } from './register-form/register-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client-brainmusic/src/app/register-form/register-form.component.ts
+++ client-brainmusic/src/app/register-form/register-form.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ConfirmPasswordValidator} from "./confirm-password.validator";
+
 function comparePassword(c: AbstractControl) {
   const v = c.value;
-  return(v.password === v.confirmPassword) ? null : {
-      passwordnotmatch: true
-    };
+  return (v.password === v.confirmPassword) ? null : {
+    passwordnotmatch: true
+  };
 }
+
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
@@ -13,20 +16,30 @@ function comparePassword(c: AbstractControl) {
 })
 export class RegisterFormComponent implements OnInit {
   registerForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      userName: ['', [Validators.required]],
-      pwGroup: this.fb.group({
-        passWord: ['', [Validators.required, Validators.pattern(/^[a-z0-9_-]{6}$/)]],
-        confirmPassword:  ['', [Validators.required, Validators.pattern(/^[a-z0-9_-]{6}$/)]],
-      }, {validators: comparePassword}),
-      gender: ['', [Validators.required]],
-      phone: ['', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)]],
+      userName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      country: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(18)]],
+      gender: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)]]
+    }, {
+      validator: ConfirmPasswordValidator.MatchPassword
     });
   }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
   onSubmit() {
     console.log(this.registerForm);
   }
