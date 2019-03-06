f58bbad85ae34137d6656cc5295dba5693c0667f
HUNGDT <hk2t.thaihung@gmail.com>
2019-03-06 09:47:50 +0700

    send again full rest service feedback

---
+++ feedback/src/main/java/com/horsemen/feedback/FeedbackApplication.java
@@ -0,0 +1,15 @@
+package com.horsemen.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class FeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackApplication.class, args);
+    }
+
+}
