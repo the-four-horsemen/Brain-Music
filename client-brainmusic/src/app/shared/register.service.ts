81a9e84772980c3eedb4c04155e3d8711704bc39
ducphu4893 <phu2638@gmail.com>
2019-03-07 13:40:03 +0700

    update homepage and router

--- client-brainmusic/src/app/shared/register.service.ts
+++ client-brainmusic/src/app/shared/register.service.ts
@@ -1,7 +1,8 @@
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {User} from './user.model';
-import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
